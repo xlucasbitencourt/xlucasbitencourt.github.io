9eeb3db7869f7282aa4ab661989a2007204a3f7a
Lucas Bitencourt <lblukz@aol.com>
2022-07-03 22:05:15 -0300

    Atualiza readme
    deleta arquivos desnecessários

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from "react";
-import "./App.css";
 import Lucas from "./components/Lucas";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
-// import { Button, Navbar } from "react-bulma-components";
 import { Route, Switch } from "react-router-dom";
 import Habilidades from "./pages/Habilidades";
 import Contato from "./pages/Contato";
