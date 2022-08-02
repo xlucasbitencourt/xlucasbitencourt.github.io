6ed0312e91e2d48d4665706fd42ab71df95dd222
Lucas Bitencourt <lblukz@aol.com>
2022-08-01 21:23:04 -0300

    Adiciona projeto Shopping Cart

--- src/docs/listaProjetos.js
+++ src/docs/listaProjetos.js
@@ -1,6 +1,7 @@
 import wallet from "../img/wallet.png";
 import trivia from "../img/trivia.png";
 import cats from "../img/cats.png";
+import shoppingCart from "../img/shopping-cart.png";
 
 // Atualize aqui a lista de projetos
 // Caso queira definir a ordem, mude aqui, ou altere "order" no css
@@ -17,7 +18,7 @@ const listaProjetos = [
   {
     nome: "Trivia",
     descricao:
-      "Um jogo de perguntas e respostas, com pontuação, ranking, utilizando a API Open Trivia DB",
+      "Um jogo de perguntas e respostas, com pontuação, ranking, utilizando a API Open Trivia DB.",
     link: "https://lucasbitencourt.com.br/tp-trivia/",
     github: "https://github.com/xlucasbitencourt/tp-trivia",
     img: trivia,
@@ -29,6 +30,13 @@ const listaProjetos = [
     link: "https://lucasbitencourt.com.br/lb-cats/",
     github: "https://github.com/xlucasbitencourt/lb-cats",
     img: cats,
+  },
+  {
+    nome: 'Shopping Cart',
+    descricao: 'Uma simulação de um carrinho de compras, feito utilizando a API do Mercado Livre.',
+    link: "https://lucasbitencourt.com.br/tp-shopping-cart/",
+    github: "https://github.com/xlucasbitencourt/tp-shopping-cart",
+    img: shoppingCart,
   },
 ];
 
